409330b1f190887b7875b3d5f731ae6c9c6782eb
ben <bjelinek42@gmail.com>
2022-03-01 12:39:41 -0700

    add file

---
+++ store_item.rb
@@ -0,0 +1,11 @@
+disc1 = {size: "185g", color: "white", price: 15}
+disc2 = {size: "190g", color: "black", price: 20}
+disc3 = {size: "200g", color: "green", price: 12}
+
+p "Disc 1 is #{disc1[:size]}, is the color #{disc1[:color]}, and costs $#{disc1[:price]}."
+
+
+p "Disc 2 is #{disc2[:size]}, is the color #{disc2[:color]}, and costs $#{disc2[:price]}."
+
+
+p "Disc 3 is #{disc3[:size]}, is the color #{disc3[:color]}, and costs $#{disc3[:price]}."
